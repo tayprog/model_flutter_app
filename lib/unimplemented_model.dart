03a8946643c0d6783dffb7dc8586736f051838bd
André Luferat <33375030+Luferat@users.noreply.github.com>
2025-06-26 19:07:03 -0300

    Update unimplemented_model.dart

--- lib/unimplemented_model.dart
+++ lib/unimplemented_model.dart
@@ -3,13 +3,10 @@ import 'package:flutter/material.dart';
 // Página vazia (Não implementada),
 // usada apenas para "demarcar" páginas futuras.
 class TestPage extends StatelessWidget {
-  // Construtor da classe TestPage.
   const TestPage({super.key});
 
   @override
-  // O método build para TestPage não está implementado, o que fará com que um erro seja lançado se a página for acessada.
   Widget build(BuildContext context) {
-    // TODO: implement build // Este comentário indica que a implementação está pendente.
     throw UnimplementedError(); // Lança um erro indicando que a funcionalidade não foi implementada.
   }
 }
